bb4f716309d00ab3e0e5c90294d380914868b4e2
ajmalrishad <ajmalaju618@gmail.com>
2025-02-16 17:47:46 +0530

    implemented get holiday search by name api and finshed backend tasks

--- holidays/urls.py
+++ holidays/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_holidays
+from .views import get_holidays,search_holidays_by_names
 
 urlpatterns = [
     path('holidays/', get_holidays, name='get_holidays'),
+    path('search_holidays_by_name/', search_holidays_by_names, name='search_holidays_by_name'),
+
 ]
--- holidays/views.py
+++ holidays/views.py
@@ -33,3 +33,37 @@ def get_holidays(request):
         cache.set(cache_key, data, timeout=86400) 
         return Response(data)
     return Response({"error": "Failed to fetch data"}, status=500)
+
+#custom api for search holiday by name
+@api_view(['GET'])
+def search_holidays_by_names(request):
+    country = request.GET.get('country')
+    year = request.GET.get('year')
+    search_query = request.GET.get('type', '').lower()  # Search by holiday name (optional)
+    if not country or not year:
+        return Response({"error": "Country and Year are required"}, status=400)
+
+    cache_key = f"holidays_{country}_{year}"
+    holidays_data = cache.get(cache_key)
+
+    if not holidays_data:
+        params = {
+            "api_key": CALENDARIFIC_API_KEY,
+            "country": country,
+            "year": year,
+        }
+        response = requests.get(BASE_URL, params=params)
+        if response.status_code != 200:
+            return Response({"error": "Failed to fetch data"}, status=500)
+
+        holidays_data = response.json()
+        cache.set(cache_key, holidays_data, timeout=86400)  # Cache for 24 hours
+
+    # Apply search filter if a query is provided
+    if search_query:
+        holidays_data["response"]["holidays"] = [
+            holiday for holiday in holidays_data["response"]["holidays"]
+            if search_query in holiday["name"].lower()
+        ]
+
+    return Response(holidays_data)
