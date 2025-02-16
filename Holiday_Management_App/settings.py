9df2228769943095f0fe2dcbe284f7e9d67a3f3b
ajmalrishad <ajmalaju618@gmail.com>
2025-02-16 15:54:13 +0530

    restframework initial setup done and db added and implemented git ignore

--- Holiday_Management_App/settings.py
+++ Holiday_Management_App/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'holidays',
 ]
 
 MIDDLEWARE = [
